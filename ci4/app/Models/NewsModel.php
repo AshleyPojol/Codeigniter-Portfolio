ccbd0ef3756aa6ee1e6344bf6e7a92a291353467
Ashii <acpojol@student.apc.edu.ph>
2023-03-01 22:45:36 +0800

    Personal [1]

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 
 class NewsModel extends Model
 {
-    protected $table = 'news';
+    protected $table = 'acpojol_news';
 	
 	protected $allowedFields = ['title', 'slug', 'body'];
 	
